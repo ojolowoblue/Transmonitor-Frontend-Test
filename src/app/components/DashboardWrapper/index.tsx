b09cca58f7e186a113ee9a8dd21674b71d634320
ojolowoblue <ojolowoblue@gmail.com>
2022-11-27 19:59:07 +0100

    update import

--- src/app/components/DashboardWrapper/index.tsx
+++ src/app/components/DashboardWrapper/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 import { links } from "./links";
-import DashboardHeader from "../DashboardHeader";
+import DashboardHeader from "../Dashboardheader";
 import {
   ChildrenWrapper,
   DashboardWrapperContainer,
