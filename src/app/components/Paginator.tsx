24d91bafe5f2c52c05caf3041f1181f263fb67ca
ojolowoblue <ojolowoblue@gmail.com>
2022-11-28 03:54:20 +0100

    create mock pagination component

---
+++ src/app/components/Paginator.tsx
@@ -0,0 +1,46 @@
+import styled from "@emotion/styled";
+
+export default function Paginator() {
+  return (
+    <PaginatorWrapper>
+      <p>Showing 1 to 10 of 500 entries</p>
+
+      <ButtonGroup>
+        <button>Previous</button>
+        <button className="active">1</button>
+        <button>2</button>
+        <button>Next</button>
+      </ButtonGroup>
+    </PaginatorWrapper>
+  );
+}
+
+export const PaginatorWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 31px;
+
+  p {
+    color: #000000;
+    font-size: 13px;
+  }
+`;
+
+export const ButtonGroup = styled.div`
+  display: flex;
+  align-items: center;
+
+  button {
+    height: 29px;
+    border: 1px solid #ced0da;
+    border-radius: 2px;
+    padding: 6px;
+    min-width: 31px;
+    font-size: 13px;
+  }
+
+  button.active {
+    background: #1875f0;
+    color: #fff;
+  }
+`;
