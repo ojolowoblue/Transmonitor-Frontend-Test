599b792630cc0648f96f34febd16f753b7dcbfac
ojolowoblue <ojolowoblue@gmail.com>
2022-11-28 01:29:44 +0100

    implement stat cards

---
+++ src/app/views/components/StatsList.tsx
@@ -0,0 +1,72 @@
+import * as React from "react";
+import styled from "@emotion/styled";
+
+import { ReactComponent as SmallChart } from "app/assets/icons/small-chart.svg";
+
+const stats: { name: string; amount: string }[] = [
+  {
+    name: "Daily Transaction Volume",
+    amount: "2,342",
+  },
+  {
+    name: "Daily Transaction Volume",
+    amount: "2,342",
+  },
+  {
+    name: "Daily Transaction Volume",
+    amount: "2,342",
+  },
+  {
+    name: "Daily Transaction Volume",
+    amount: "2,342",
+  },
+];
+
+export const Stat = ({ name, amount }: { name: string; amount: string }) => {
+  return (
+    <StatWrapper>
+      <div>
+        <small className="name">{name}</small>
+        <p className="amount">{amount}</p>
+      </div>
+
+      <SmallChart />
+    </StatWrapper>
+  );
+};
+
+export default function StatsList() {
+  return (
+    <StatsWrapper>
+      {stats.map((stat, idx) => (
+        <Stat name={stat.name} amount={stat.amount} key={idx.toString()} />
+      ))}
+    </StatsWrapper>
+  );
+}
+
+export const StatsWrapper = styled.div`
+  display: flex;
+  gap: 9px;
+`;
+
+export const StatWrapper = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  padding: 16px 18px;
+  background: #ffffff;
+  border: 1px solid #eef8fd;
+  border-radius: 2px;
+
+  .name {
+    color: #787c90;
+    font-size: 12px;
+  }
+
+  .amount {
+    color: #262626;
+    font-size: 18px;
+  }
+`;
